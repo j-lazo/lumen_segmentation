7de61c0762a1f22dcbb19d919bab3359bda10f04
jorge <j.org.ls@ciencias.unam.mx>
2021-07-05 19:18:42 +0200

    edited to handle ensemble models

--- models/call_models.py
+++ models/call_models.py
@@ -13,10 +13,11 @@ import os
 import csv
 import matplotlib.pyplot as plt
 from Unet_based import ResUnet
-from Unet_based import Transpose_Unet
 from Unet_based import Transpose_ResUnet
 from Unet_based import Transpose_Unet
 from Unet_based import Unet
+from Unet_based import continuous_blocks_ResUnet
+from Unet_based import ensemble
 from sklearn.metrics import average_precision_score
 from sklearn.metrics import recall_score
 from sklearn.metrics import accuracy_score
@@ -46,13 +47,6 @@ def load_data_only_imgs(path):
     return (images, images)
 
 
-def read_image_test(path):
-    x = cv2.imread(path, cv2.IMREAD_COLOR)
-    x = cv2.resize(x, (256, 256))
-    x = x / 255.0
-    return x
-
-
 def read_mask_test(path):
     x = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
     x = cv2.resize(x, (256, 256))
@@ -66,6 +60,27 @@ def read_image(path):
     x = cv2.imread(path, 1)
     x = cv2.resize(x, (256, 256))
     x = x/255.0
+    return x
+
+
+def read_image_and_npy(path):
+    path = path.decode()
+    x_vol = np.load(path, allow_pickle=True)
+    x_vol = np.resize(x_vol, (3, 256, 256, 3))
+    x_vol = x_vol / 255.0
+    x_frame = x_vol[0]
+    x_frame = cv2.resize(x_frame, (256, 256))
+    x_frame = x_frame / 255.0
+
+    return x_vol, x_frame
+
+
+def read_image_npy(path):
+    path = path.decode()
+    x = np.load(path, allow_pickle=True)
+    x = np.resize(x, (3, 256, 256, 3))
+    x = x / 255.0
+
     return x
 
 
@@ -91,9 +106,42 @@ def tf_parse(x, y):
     return x, y
 
 
+def tf_parse_v2(x, y):
+    def _parse(x, y):
+        x = read_image_npy(x)
+        y = read_mask(y)
+        return x, y
+
+    x, y = tf.numpy_function(_parse, [x, y], [tf.float64, tf.float64])
+    x.set_shape([3, 256, 256, 3])
+    y.set_shape([256, 256, 1])
+    return x, y
+
+
+def tf_parse_v3(x, y):
+    def _parse(x, y):
+        x_vol, x_frame = read_image_and_npy(x)
+        y = read_mask(y)
+
+        return x_vol, x_frame, y
+
+    x_vol, x_frame, y = tf.numpy_function(_parse, [x, y], [tf.float64, tf.float64, tf.float64])
+    x_vol.set_shape([3, 256, 256, 3])
+    x_frame.set_shape([256, 256, 3])
+    y.set_shape([256, 256, 1])
+    return (x_vol, x_frame), y
+
+
 def tf_dataset(x, y, batch=8, img_modality='rgb'):
     dataset = tf.data.Dataset.from_tensor_slices((x, y))
-    dataset = dataset.map(tf_parse)
+
+    if img_modality == 'npy':
+        dataset = dataset.map(tf_parse_v2)
+    elif img_modality == 'ensemble':
+        dataset = dataset.map(tf_parse_v3)
+    else:
+        dataset = dataset.map(tf_parse)
+
     dataset = dataset.batch(batch)
     dataset = dataset.repeat()
     return dataset
@@ -140,6 +188,12 @@ def build_model(model_name):
     elif model_name == 'Unet':
         model = Unet.build_model()
 
+    elif model_name == 'continuous_blocks_ResUnet':
+        model = continuous_blocks_ResUnet.build()
+
+    elif model_name == 'ensemble':
+        model = ensemble.build_model()
+
     return model
 
 
@@ -150,10 +204,26 @@ def mask_parse(mask):
     return mask
 
 
-def read_image_test(path):
-    x = cv2.imread(path, cv2.IMREAD_COLOR)
-    x = cv2.resize(x, (256, 256))
-    x = x / 255.0
+def read_image_test(path, img_modality='rgb'):
+    if img_modality == 'npy':
+        x = np.load(path, allow_pickle=True)
+        x = np.resize(x, (3, 256, 256, 3))
+        x = x / 255.0
+
+    elif img_modality == 'ensemble':
+        x_vol = np.load(path, allow_pickle=True)
+        x_vol = np.resize(x_vol, (3, 256, 256, 3))
+        x_vol = x_vol / 255.0
+        x_frame = x_vol[0]
+        x_frame = cv2.resize(x_frame, (256, 256))
+        x_frame = x_frame / 255.0
+        x =  x_vol, x_frame
+
+    else:
+        x = cv2.imread(path, cv2.IMREAD_COLOR)
+        x = cv2.resize(x, (256, 256))
+        x = x / 255.0
+
     return x
 
 
@@ -284,8 +354,10 @@ def evaluate_and_predict(model, directory_to_evaluate,
 
     output_directory = 'predictions/' + output_name + '/'
     batch_size = 8
+    print(image_modality)
     (test_x, test_y) = load_data(directory_to_evaluate, image_modality)
-    test_dataset = tf_dataset(test_x, test_y, batch=batch_size)
+    test_dataset = tf_dataset(test_x, test_y, batch=batch_size,
+                              img_modality=image_modality)
     test_steps = (len(test_x)//batch_size)
 
     if len(test_x) % batch_size != 0:
@@ -293,20 +365,27 @@ def evaluate_and_predict(model, directory_to_evaluate,
 
     # evaluate the model in the test dataset
     model.evaluate(test_dataset, steps=test_steps)
-    test_steps = (len(test_x)//batch_size)
-    if len(test_x) % batch_size != 0:
-        test_steps += 1
     times = []
     for i, (x, y) in tqdm(enumerate(zip(test_x, test_y)), total=len(test_x)):
-        #print(i, x)
         directory_image = x
-        x = read_image_test(x)
-        #y = read_mask_test(y)
         init_time = time.time()
-        y_pred = model.predict(np.expand_dims(x, axis=0))[0] > 0.5
+        x = read_image_test(x, image_modality)
+        if image_modality == 'ensemble':
+            x_vol = np.expand_dims(x[0], axis=0)
+            x_frame = np.expand_dims(x[1], axis=0)
+            print(np.shape(x_vol))
+            print(np.shape(x_frame))
+            x = x_vol, x_frame
+        else:
+            x = np.expand_dims(x, axis=0)
+
+        y_pred = model.predict(x)[0] > 0.5
         delta = time.time() - init_time
         times.append(delta)
         name_original_file = directory_image.replace(''.join([directory_to_evaluate, 'image/']), '')
+
+        if image_modality == 'npy' or image_modality == 'ensemble':
+            name_original_file = name_original_file.replace('.npy', '.png')
         results_name = ''.join([results_directory, output_directory, name_original_file])
         cv2.imwrite(results_name, y_pred * 255.0)
 
@@ -321,9 +400,10 @@ def evaluate_and_predict(model, directory_to_evaluate,
     results_sensitivity = []
     results_specificity = []
     output_directory = 'predictions/' + output_name + '/'
-    batch_size = 16
+    batch_size = 8
     (test_x, test_y) = load_data(directory_to_evaluate, image_modality)
-    test_dataset = tf_dataset(test_x, test_y, batch=batch_size)
+    test_dataset = tf_dataset(test_x, test_y, batch=batch_size,
+                              img_modality=image_modality)
     test_steps = (len(test_x) // batch_size)
 
     # save the results of the test dataset in a CSV file
@@ -341,11 +421,10 @@ def evaluate_and_predict(model, directory_to_evaluate,
     results_accuracy = []
 
     for image in ground_truth_image_list[:]:
-
-        result_image = [name for name in results_image_list if image[-12:] == name[-12:]][0]
+        result_image = [name for name in results_image_list if image[:-4] == name[:-4]][0]
         if result_image is not None:
-
-            original_mask = read_img(''.join([ground_truth_labels_dir, image]))
+            image_name = image[:-4]
+            original_mask = read_img(''.join([ground_truth_labels_dir, image_name, '.png']))
             predicted_mask = read_img(''.join([result_mask_dir, result_image]))
             dice_val = dice(original_mask, predicted_mask)
             results_dice.append(dice_val)
@@ -381,13 +460,6 @@ def evaluate_and_predict(model, directory_to_evaluate,
     return name_test_csv_file
 
 
-def predict_mask(model, input_image):
-
-    y_pred = model.predict(np.expand_dims(input_image, axis=0))[0] >= 0.5
-
-    return y_pred
-
-
 def paint_imgs(img, mask):
 
     if np.shape(img) != np.shape(mask):
@@ -399,71 +471,6 @@ def paint_imgs(img, mask):
                 img[i, j, 1] = 100
 
     return img
-
-
-def build_contours(array_of_points):
-
-    contours = []
-    for i, y_points in enumerate(array_of_points[0]):
-        point = (array_of_points[1][i], y_points)
-        point = np.asarray(point)
-        contours.append([point])
-
-    return contours
-
-
-def calc_histograms_and_center(mask, image):
-
-    if not (np.all(mask == 0)):
-
-        percentage = 0.6
-        #grayscale = cv2.cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        grayscale = image[:, :, 2]
-        grayscale = np.multiply(grayscale, mask)
-
-        #list_values_grayscale = [value for row in grayscale for value in row if value != 0]
-        # create the histogram plot, with three lines, one for
-        # each color
-        max_grays = ((np.where(grayscale >= int(percentage * np.amax(grayscale)))))
-        gray_contours = np.asarray([build_contours(max_grays)])
-        gray_convex_hull, gray_x, gray_y = determine_convex_hull(gray_contours)
-
-        points_x = []
-        points_y = []
-        for hull in gray_convex_hull:
-            for i, point in enumerate(hull):
-                points_x.append(point[0][0])
-                points_y.append(point[0][1])
-
-    else:
-        gray_x = 'nAN'
-        gray_y = 'nAN'
-
-    return gray_x, gray_y
-
-
-def detect_dark_region(mask, image):
-    w_image, h_image, d_image = np.shape(image)
-    w_mask, h_mask, d_mask = np.shape(mask)
-    temp_mask = np.zeros((w_mask, h_mask, 3), dtype="float32")
-    temp_mask[:, :, 0] = mask[:, :, 0]
-    temp_mask[:, :, 1] = mask[:, :, 0]
-    temp_mask[:, :, 2] = mask[:, :, 0]
-
-    if w_image != w_mask or h_image != h_mask:
-       mask = cv2.resize(temp_mask, (w_image, h_image))
-
-    gt_mask = np.zeros((w_image, h_image))
-
-    if np.any(mask):
-
-        #temp_mask = cv2.cv2.cvtColor(temp_mask*1, cv2.COLOR_BGR2GRAY)
-        cl_mask = clean_mask(mask*1)
-        gt_mask[cl_mask == 1.0] = 1
-
-    point_x, point_y = calc_histograms_and_center(gt_mask, image)
-
-    return point_x, point_y
 
 
 def clean_mask(mask):
@@ -500,40 +507,6 @@ def clean_mask(mask):
         new_mask = mask
 
     return new_mask
-
-
-def determine_convex_hull(contours):
-
-    point_sets = np.asarray(contours[0])
-    #contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    # create hull array for convex hull points
-    new_hulls = []
-    new_contours = []
-    # calculate points for each contour
-    if len(point_sets) > 1:
-        temp_contours = point_sets[0]
-        for i in range(1, len(point_sets)):
-            temp_contours = np.concatenate((temp_contours, point_sets[i]), axis=0)
-
-        new_contours.append(temp_contours)
-    else:
-        new_contours = point_sets
-
-    for i in range(len(new_contours)):
-        new_hulls.append(cv2.convexHull(new_contours[i], False))
-
-    if new_hulls == []:
-        point_x = 'old'
-        point_y = 'old'
-    else:
-        M = cv2.moments(new_hulls[0])
-        if M["m00"] < 0.001:
-            M["m00"] = 0.001
-
-        point_x = int(M["m10"] / M["m00"])
-        point_y = int(M["m01"] / M["m00"])
-
-    return new_hulls, point_x, point_y
 
 
 def save_history(name_performance_metrics_file, model_history):
@@ -598,8 +571,7 @@ def save_plots(model_history, results_directory, new_results_id):
 
 
 def main(project_folder, name_model, batch, lr):
-
-    epochs = 450
+    epochs = 650
     # optimizer:
     opt = tf.keras.optimizers.Adam(lr)
 
@@ -612,13 +584,29 @@ def main(project_folder, name_model, batch, lr):
         amount_data = '/original_data/'
 
     # Define training and validation data
-    path = project_folder
     train_data_used = ''.join([project_folder, 'train', amount_data])
+    val_data_used = ''.join([project_folder, 'val', amount_data])
+
+    if name_model == 'continuous_blocks_ResUnet':
+        image_modality = 'npy'
+        amount_data = '/original_data/'
+        train_data_used = ''.join([project_folder, 'volume_data/', str(3), '_continuous_frames/',
+                                   'train', amount_data])
+        val_data_used = ''.join([project_folder, 'volume_data/', str(3), '_continuous_frames/',
+                                 'val', amount_data])
+    elif name_model == 'ensemble':
+        image_modality = 'ensemble'
+        amount_data = '/original_data/'
+        train_data_used = ''.join([project_folder, 'volume_data/', str(3), '_continuous_frames/',
+                                   'train', amount_data])
+        val_data_used = ''.join([project_folder, 'volume_data/', str(3), '_continuous_frames/',
+                                 'val', amount_data])
+
+
 
     (train_x, train_y) = load_data(train_data_used, image_modality)
     print('Data training: ', train_data_used)
 
-    val_data_used = ''.join([project_folder, 'val', amount_data])
     (valid_x, valid_y) = load_data(val_data_used, image_modality)
     print('Data validation: ', val_data_used)
 
@@ -700,7 +688,11 @@ def main(project_folder, name_model, batch, lr):
     for folder in list_of_test_sets:
         names_csv_files = []
         os.mkdir(''.join([results_directory, 'predictions/', folder, '/']))
-        evaluation_directory = ''.join([project_folder, 'test/', folder, '/'])
+        if image_modality == 'npy' or image_modality == 'ensemble':
+            evaluation_directory = ''.join([project_folder, 'volume_data/', str(3), '_continuous_frames/',
+                                       'test/', folder, '/'])
+        else:
+            evaluation_directory = ''.join([project_folder, 'test/', folder, '/'])
         name_test_csv_file = evaluate_and_predict(model, evaluation_directory,
                                                   image_modality,
                                                   results_directory, folder, new_results_id)
@@ -708,25 +700,33 @@ def main(project_folder, name_model, batch, lr):
 
     if analyze_validation_set is True:
         os.mkdir(results_directory + 'predictions/val/')
-        evaluation_directory_val = project_folder + "val/original_data/"
+        if image_modality == 'npy' or image_modality == 'ensemble':
+            evaluation_directory = ''.join([project_folder, 'volume_data/', str(3), '_continuous_frames/',
+                                            'val/original_data/'])
+        else:
+            evaluation_directory_val = project_folder + "val/original_data/"
         name_test_csv_file = evaluate_and_predict(model, evaluation_directory_val,
                                                     image_modality, results_directory,
                                                   'val', new_results_id)
 
     if evaluate_train_dir is True:
         os.mkdir(results_directory + 'predictions/train/')
-        evaluation_directory_val = project_folder + "train/original_data/"
+        if image_modality == 'npy' or image_modality == 'ensemble':
+            evaluation_directory = ''.join([project_folder, 'volume_data/', str(3), '_continuous_frames/',
+                                            'train/original_data/'])
+        else:
+            evaluation_directory_val = project_folder + "train/original_data/"
         name_test_csv_file = evaluate_and_predict(model, evaluation_directory_val,
                                                   image_modality, results_directory,
                                                   'train', new_results_id)
 
 
 if __name__ == "__main__":
-    project_folder = '/home/nearlab/Jorge/current_work/lumen_segmentation/data/phantom_lumen/'
 
-    name_models = ['ResUnet', 'Unet', 'Transpose_Unet', 'Transpose_ResUnet']
+    project_folder = '/home/nearlab/Jorge/current_work/lumen_segmentation/data/phantom_lumen/'
+    name_models = ['ensemble']
     # Hyper-parameters:
-    batches = [4, 8, 16]
+    batches = [8, 16, 4, 32]
     learing_rates = [1e-3, 1e-4, 1e-5, 1e-6]
     for name_model in name_models:
         for batch in batches:
